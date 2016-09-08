27bca3cf1e899e5c26e77c8c1c9a90a95aa3e74f
Keegan Alexander Mullins <mullinska@dcsdk12.org>
2016-09-08 13:38:56 -0600

    tests for has_teen?

--- methods_quiz_test.rb
+++ methods_quiz_test.rb
@@ -3,3 +3,23 @@ require './methods_quiz'
 
 # TODO - write tests here
 
+describe 'Methods' do
+  describe 'has_teen?' do
+    it "should test if a number is between 13 and 19, if one of 3 ints is a teen: should return true" do
+      has_teen?(5, 14, 21).must_equal(true)
+    end
+    it "should test if a number is between 13 and 19, if none of 3 ints is a teen: should return false" do
+      has_teen?(5, 13, 19).must_equal(true)
+    end
+    it "should test if a number is between 13 and 19, if one of 3 ints is 13 or 19: should return true" do
+      has_teen?(5, 13, 19).must_equal(true)
+    end
+  end
+
+
+
+
+
+
+
+end
